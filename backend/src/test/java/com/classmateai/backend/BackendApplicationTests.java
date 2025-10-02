ebc346aa3878e520947e8f16b35a5b9c8b42fae4
Nicolás <durregob@unal.edu.co>
2025-10-02 01:13:19 -0500

    Se agrega estructura backend inicial con Spring Boot (Web, JPA, Security, Lombok, PostgreSQL

---
+++ backend/src/test/java/com/classmateai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classmateai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
